d751a2f3d528f0773add7a541f758beb3fb67506
fukumotokiraa <122710kisose@gmail.com>
2024-12-23 09:36:54 +0900

    プロジェクト ファイルを追加します。

---
+++ TitleScene.cpp
@@ -0,0 +1,18 @@
+#include "TitleScene.h"
+
+void TitleScene::Initialize() {}
+
+void TitleScene::Update(char* keys, char* preKeys) {
+
+	//スペースキーを押した瞬間
+	if (keys[DIK_SPACE] && preKeys[DIK_SPACE] == false) {
+		//シーンをSTAGEに切り替える
+		IScene::sceneNo = STAGE;
+	}
+}
+
+void TitleScene::Draw() {
+	Novice::ScreenPrintf(0, 0, "Title Scene");
+	Novice::ScreenPrintf(0, 30, "Push Space");
+}
+
